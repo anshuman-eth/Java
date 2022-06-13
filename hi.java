904072e5354fdbccda2969556e14b46d7849b885
Anshuman Singh4 <AS00844638@techmahindra.com>
2022-06-14 02:18:23 +0530

    initial commit

---
+++ hi.java
@@ -0,0 +1,11 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int i=5;
+        int j=6;
+        int k=2;
+        int add=i+j+k;
+        System.out.println(add);
+    }
+}
---
+++ literals.java
@@ -0,0 +1,21 @@
+import javax.print.event.PrintEvent;
+
+public class literals {
+    public static void main(String[] args) {
+        int age = 34;
+        byte age2 =55;    // byte uto 127 
+        long long_number = 2342345325334l;   // if number is too larger add 'l/L' at the end
+        short age3 = 100;         // ideal for short numbers
+        char ch = '#';
+        float f1 = 5.6f;
+        double d1 = 4.66;    // you can also use 'd/D' at the end if you want
+        boolean a = true;
+        System.out.println(long_number);
+        String name = "Anshuman Singh";
+        System.out.print(name);
+
+
+
+    }
+    
+}
