7d71eb784b9b61fb50e09c79a6657b1b62579b3a
Anshuman Singh4 <AS00844638@techmahindra.com>
2022-06-16 13:29:45 +0530

    sum of int & double

---
+++ sum.java
@@ -0,0 +1,10 @@
+// Your First Program
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 1;
+        double b =0.32423;
+        double d = a + b;
+        System.out.println("The sum is "+ d);
+    }
+}
